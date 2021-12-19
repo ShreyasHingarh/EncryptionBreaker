375cdf52763e3d7220b15e565af4d8d4b1410240
ShreyasHingarh <shreyas.hingarh2@gmail.com>
2021-12-19 11:32:35 -0800

    Add project files.

---
+++ EncryptionBreaker/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace EncryptionBreaker
+{
+    class Program
+    {
+        //Make a function that takes in an encrypted string 
+        //If the character is special (if it is a space, exclamation mark or anything that is not a letter, ignore it)
+        //return a List<string> which represents all of the possible decryptions of the string'
+        string possibleDecryption(int jump, string encryption)
+        {
+            string decryption
+            for(int i = 0;i < encryption.Length;i++)
+            {
+                if)
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
